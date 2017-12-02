817c016dcc2798b08dabdc9df03cfd875887a85f
Julian Lange <langj664@newschool.edu>
2017-12-02 23:00:13 +0000

    add new final01 assignment files

---
+++ final01-assignment/final01-assignment-part3-final.js
@@ -0,0 +1,37 @@
+// enter into EC2 instance
+// use Linux command in email to connect with mongo shell
+// go to database with command: use julian
+// create new collection with command: db.createCollection('meetings')
+// quit()
+// enter url as environment variable
+
+var request = require('request');
+
+// var dbName = 'julian';
+var collName = 'meetings';
+
+request('https://raw.githubusercontent.com/julianhlange/data-structures/master/final01-assignment/addressarraywithlatLongv2.json', function(error, response, body) {
+    var meetingData = JSON.parse(body);
+    // console.log(meetingData);
+
+    // Connection URL
+    // var url = fs.readFileSync('./../ATLAS.txt', 'utf8');
+    // console.log(url);
+    var url = process.env.ATLAS;
+    
+
+    // Retrieve
+    var MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) {return console.dir(err);}
+
+        var collection = db.collection(collName);
+
+        // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+        collection.insert(meetingData);
+        db.close();
+
+    });
+
+});
